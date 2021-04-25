1a16ddc5d58b53129f0b22abcb24be33b087373e
Nerlih Max <nerlihmax@yandex.ru>
2021-04-25 21:35:51 +0700

    работает с .docx

---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+import xml.etree.ElementTree as ET
+import random
+import sys
+
+def word(filename):
+    os.system(f'unzip {filename}')
+
+    new_application_name = str(random.randint(0, 100))
+    new_creator = str(random.randint(0, 100))
+    new_last_edit_by = str(random.randint(0, 100))
+    new_create_time = str(random.randint(0, 100))
+    new_change_time = str(random.randint(0, 100))
+
+    tree = ET.parse('docProps/app.xml')
+    root = tree.getroot()
+
+    #Application Name
+    app_application_offset = 5
+    element = root[app_application_offset]
+    application_old = element.text
+    element.text = new_application_name
+    tree.write('docProps/app.xml')
+    print(f'changed Application Name from {application_old} to {element.text}')
+
+    tree = ET.parse('docProps/core.xml')
+    root = tree.getroot()
+
+    #Creator
+    core_creator_offset = 2
+    element = root[core_creator_offset]
+    creator_old = element.text
+    element.text = new_creator
+    print(f'changed Creator from {creator_old} to {element.text}')
+
+    #Last Edited by
+    core_last_edit_by = 5
+    element = root[core_last_edit_by]
+    last_edit_by_old = element.text
+    element.text = new_last_edit_by
+    tree.write('docProps/core.xml')
+    print(f'changed Last Edited by from {last_edit_by_old} to {element.text}')
+
+    #Create time
+    core_last_edit_by = 7
+    element = root[core_last_edit_by]
+    create_time_old = element.text
+    element.text = new_create_time
+    tree.write('docProps/core.xml')
+    print(f'changed Last Edited by from {create_time_old} to {element.text}')
+
+    #Edition time
+    core_last_edit_by = 8
+    element = root[core_last_edit_by]
+    change_time_old = element.text
+    element.text = new_change_time
+    tree.write('docProps/core.xml')
+    print(f'changed Last Edited by from {change_time_old} to {element.text}')
+
+    os.system('zip edited.docx \[Content_Types\].xml word/ _rels/ docProps/ -r')
+    os.system('rm -rf _rels/ docProps/ word/ \[Content_Types\].xml')
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    word(filename)
